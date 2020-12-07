af78c6413a29130ef192b48692d4e930a56a6624
Willem Sporen <willem.sporen@scenius.nl>
2020-12-07 11:37:14 +0100

    Scaffold Day 7

---
+++ Day7.HandyHaversacks/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+open System.IO
+
+[<EntryPoint>]
+let main argv =
+    match Array.tryHead argv with
+    | Some path ->
+        printfn "Day 7 - Handy Haversacks ()"
+        printfn "Using path: %s" path
+        0 // return an integer exit code
+    | _ -> 0
