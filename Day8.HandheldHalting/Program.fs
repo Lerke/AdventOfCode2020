e35d6ba66234af257ddab832949e2c2673433cf9
Willem Sporen <willem.sporen@scenius.nl>
2020-12-08 21:09:39 +0100

    Day 8 - Two stars

--- Day8.HandheldHalting/Program.fs
+++ Day8.HandheldHalting/Program.fs
@@ -7,6 +7,13 @@ type Operation =
     | Jump of int
     | Nop of int
 
+// The handheld can be in one of the following states
+type ExecutionState =
+    | NotStarted    // Initial State
+    | Running       // Program still executing
+    | HaltDueToLoop // Abnormal halt - It's spinlocking!
+    | EndOfProgram  // Normal halt - No more program left!
+
 // Input text is parsed to operations using a regular expression
 type PatternRegex = Regex< @"^(?<operation>\w{3})\s(?<argument>(?:\+|-)\d+)$" >
 
@@ -24,16 +31,27 @@ type HandheldConsole = {
     MemoryMap: MemoryLocation []
     Accumulator: int
     CycleCount: int
+    ExecutionState: ExecutionState
 }
 
 // Pretty print the state of the console
 let PrintHandheldState console =
     printfn
-        "Handheld Console ---\nInstruction Pointer: %d\nAccumulator: %d\nInstructions: %d\nCycles Simulated: %d"
+        "--- Handheld Console ---\nInstruction Pointer: %d\nAccumulator: %d\nInstructions: %d\nCycles Simulated: %d"
         console.InstructionPointer
         console.Accumulator
         (console.MemoryMap |> Array.length)
         console.CycleCount
+
+
+// Swaps a NOP -> JMP and a JMP -> NOP at a certain index.
+// Returns the new memory map
+let SwapMemory memoryMap index =
+    memoryMap
+    |> Array.mapi (fun i x -> { x with operation = (match x.operation with
+                                                  | Jump x when i = index -> Nop x
+                                                  | Nop x when i = index -> Jump x
+                                                  | x -> x ) })
 
 // Update the amount of times a certain memory position was visited by appending the
 // current IP location.
@@ -49,17 +67,20 @@ let rec RunProgram console cpuCycle =
             match ((console.MemoryMap.[console.InstructionPointer])).operation with
             // For ACC, increase the accumulator by the argument, then increase the IP by 1
             | Accumulate x -> RunProgram ({ console with
+                                                ExecutionState = Running
                                                 MemoryMap = (UpdateMemoryVisits console.MemoryMap console.InstructionPointer cpuCycle)
                                                 InstructionPointer = console.InstructionPointer + 1
                                                 Accumulator = console.Accumulator + x
                                                 CycleCount = cpuCycle}) (cpuCycle + 1)
             // For JMPs, just add x to the instruction pointer and keep going.
             | Jump x -> RunProgram ({ console with
+                                        ExecutionState = Running
                                         MemoryMap = (UpdateMemoryVisits console.MemoryMap console.InstructionPointer cpuCycle)
                                         InstructionPointer = console.InstructionPointer + x
                                         CycleCount = cpuCycle }) (cpuCycle + 1)
             // For NOPs, do nothing but increase the IP by 1
             | Nop x -> RunProgram ({ console with
+                                        ExecutionState = Running
                                         MemoryMap = (UpdateMemoryVisits console.MemoryMap console.InstructionPointer cpuCycle)
                                         InstructionPointer = console.InstructionPointer + 1
                                         CycleCount = cpuCycle}) (cpuCycle + 1)
@@ -70,11 +91,11 @@ let rec RunProgram console cpuCycle =
             console.MemoryMap.[console.InstructionPointer].input
             console.InstructionPointer
             console.MemoryMap.[console.InstructionPointer].visitedAt
-        console
+        { console with ExecutionState = HaltDueToLoop ; CycleCount = cpuCycle }
     | false ->
         // We ran out of program. Halt
         printfn "Halting! Reached end of memory map"
-        { console with CycleCount = cpuCycle }
+        { console with CycleCount = cpuCycle ; ExecutionState = EndOfProgram }
 
 // Turn a line of input into a typed operation
 let ParseOperation input =
@@ -88,11 +109,25 @@ let ParseOperation input =
     | _ ->
         failwithf "Unrecognized input: %s" input
 
+// Calculate all new states where some nop is replaced with jmp
+// and some jmp is replaced with nop
+let PermuteProgramWithJumpNops console =
+    // Calculate where our Jumps and Nops all are. Then for each create a new
+    // Initial program state with that specific Jmp/Nop replaced.
+    let jmpsAndNops = console.MemoryMap
+                        |> Array.mapi (fun i f -> (i, f))
+                        |> Array.fold (fun (acc: int list) ((i:int), (x: MemoryLocation)) ->
+                                       match x.operation with
+                                       | Nop _ | Jump _ -> (i :: acc)
+                                       | _ -> acc) []
+    jmpsAndNops
+    |> List.map (fun f -> { console with MemoryMap = (SwapMemory console.MemoryMap f) })
+
 [<EntryPoint>]
 let main argv =
     match argv with
     | [| path |] ->
-        printfn "Day 8 - Handheld Halting (*)"
+        printfn "Day 8 - Handheld Halting (**)"
         printfn "Using path: %s" path
         let initialConsoleState = {
             InstructionPointer = 0
@@ -101,10 +136,19 @@ let main argv =
                         |> (Array.map ParseOperation)
             Accumulator = 0 // Accumulator is initialized at 0
             CycleCount = 1 // We have not done any cycles yet.
+            ExecutionState = NotStarted
         }
         let output = RunProgram initialConsoleState 1
         PrintHandheldState output
-        printfn "Final value of accumulator before infinite loop (*): %d" output.Accumulator
+        printfn "Final value of accumulator before infinite loop (*): %d\n\n" output.Accumulator
+
+        let permutations = PermuteProgramWithJumpNops initialConsoleState
+        let computedPermutations = permutations |> List.map (fun p -> RunProgram p 1)
+        let correctProgram = computedPermutations |> List.find (fun f -> f.ExecutionState = EndOfProgram)
+
+        printfn "Correct console after permuting over possible programs"
+        PrintHandheldState correctProgram
+        printfn "Final value of accumulator for adjusted program (**): %d" correctProgram.Accumulator
         0
     | _ ->
         printfn "Usage: dotnet run ./path/to/input.txt"
