262d2bdf7f1b36036e558af4b233f9676f1216e3
Willem Sporen <willem.sporen@scenius.nl>
2020-12-09 19:19:17 +0100

    Day 9 - One star

--- Day9.EncodingError/Program.fs
+++ Day9.EncodingError/Program.fs
@@ -1,4 +1,55 @@
-﻿[<EntryPoint>]
+﻿open System
+open System.IO
+
+type XmasInput = {
+    Numbers: int64 list
+    PreambleLength: int
+}
+
+// Recursively process the list of input numbers until we find a number
+// that doesn't match the rules:
+//    - Number should be the sum of any two of the <preamble> previous numbers
+//    - There may be more than one such pair present
+//    - Pairs with equal components do not count
+let rec ValidateXmasEncoding input iteration =
+    let offset = ((iteration |> int64) - (input.PreambleLength |> int64)) |> int
+    match (offset >= 0) with
+    | true ->
+        // Check if current number matches. If not, return the number and index.
+        // Get pairs of numbers
+        let skippedList = input.Numbers
+                          |> List.skip offset
+                          |> List.take input.PreambleLength
+        match skippedList
+              |> List.allPairs(skippedList)
+              |> List.filter (fun (x, y) -> x <> y)
+              |> List.tryFind (fun (x, y) -> (x + y) = input.Numbers.[iteration]) with
+              | Some (x, y) ->
+                  // We have a pair that sums to this number. Keep going.
+                  ValidateXmasEncoding input (iteration + 1)
+              | _ ->
+                  // There's no pairs that add to this number. We've found our one star
+                  // match
+                  (input.Numbers.[iteration], iteration)
+    | false ->
+        // No check needed. We're still parsing the preamble, just continue
+        ValidateXmasEncoding input (iteration + 1)
+
+[<EntryPoint>]
 let main argv =
-    printfn "Hello world"
-    0 // return an integer exit code
+    match argv with
+    | [| path; preamble |] when fst (Int32.TryParse preamble) = true ->
+        printfn "Day 9 - Encoding Error (*)"
+        printfn "Path: %s\nPreamble: %s" path preamble
+        let initialInput = {
+            Numbers = (File.ReadAllLines path |> (Array.map (int64))) |> List.ofArray
+            PreambleLength = (preamble |> int)
+        }
+
+        let (output, iteration) = ValidateXmasEncoding initialInput 0
+
+        printfn "First number that is not a combination of any two pairs (*): %d at index %d" output iteration
+        0
+    | _ ->
+        printfn "Usage: dotnet run ./path/to/input preambleLength"
+        1
