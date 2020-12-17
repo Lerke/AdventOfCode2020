f51a5d8befff89da48b5715620b790b3842cb998
Willem <willem.sporen@scenius.nl>
2020-12-17 23:45:48 +0100

    Day 14 - Two star

--- Day14.DockingData/Program.fs
+++ Day14.DockingData/Program.fs
@@ -2,6 +2,10 @@
 open System.IO
 open FSharp.Text.RegexProvider
 
+type EmulatorVersion =
+    | One
+    | Two
+
 type BitmaskElement =
     | Ignore
     | Value of int64
@@ -10,12 +14,20 @@ type Instruction =
     | Mask of BitmaskElement []
     | Write of (int64 * int64)
 
+type MemoryLocation = {
+    Index: int64
+    Value: int64
+}
+
 type Emulator = {
-    Memory: int64 []
+    Memory: MemoryLocation list
     Bitmask: BitmaskElement []
+    Version: EmulatorVersion
 }
 
 type InstructionRegex = Regex< @"^(?<mask>mask = (?<bitmask>(X|1|0)*))$|(?<write>mem\[((?<location>\d+)\]) = (?<wval>\d+))$" >
+type FloatingRegex = Regex< @"X" >
+
 let ParseInstruction line =
     match InstructionRegex().TryTypedMatch line with
     | Some x -> match (x.mask.Success, x.write.Success) with
@@ -26,18 +38,49 @@ let ParseInstruction line =
                 | _ -> failwithf "What is this line? %s" line
     | None -> failwithf "Could not parse line: %s" line
 
-let ReplaceArrayIdx (idx: int64) value (array: int64 []) =
-    array |> Array.mapi (fun i f -> if (i |> int64) = idx then value else f)
+let ReplaceArrayIdx (idx: int64) (value: int64) (array: MemoryLocation list) =
+    match (List.tryFind (fun f -> f.Index = idx) array) with
+    | Some x -> array |> List.map (fun f -> if (f.Index) = idx then { f with Value = value } else f)
+    | None -> { Index = idx; Value = value } :: array
+
+let rec ExpandFloatingBits maskStrings =
+    maskStrings
+    |> List.collect(fun maskString ->
+        match Regex.Match(maskString, "X") with
+        | m -> match m.Success with
+               | true -> ExpandFloatingBits ([ (FloatingRegex().Replace(maskString, "1", 1)) ; FloatingRegex().Replace(maskString, "0", 1) ])
+               | false -> maskStrings)
+    |> List.distinct
+     
+let rec RecMaskedWrite (value: int64) (locations: string list) (emulator) =
+    match locations with
+    | x :: xs -> RecMaskedWrite value xs { emulator with Memory =
+                                            (ReplaceArrayIdx
+                                                 (Convert.ToInt64(x, 2))
+                                                 value
+                                            emulator.Memory) }
+    | [] -> emulator
+    
+let ApplyMask (value: int64) (mask: BitmaskElement[]) =
+    Array.zip ( Convert.ToString(value, 2).PadLeft(36, '0') |> Seq.toArray) (mask)
+        |> Array.map (fun (v, m) -> match m with
+                               | Ignore -> "X"
+                               | Value x when x = 0L -> v |> string
+                               | Value x when x <> 0L -> x |> string)
+        |> Array.toList
+        |> fun x -> String.Join("", x)
+    
 let MaskedWrite (value: int64) (location: int64) (emulator: Emulator) =
     let valueBits = Convert.ToString(value, 2).PadLeft(36, '0') |> Seq.toList
-    { emulator with Memory = ReplaceArrayIdx
-                                 location
-                                 (List.zip valueBits (emulator.Bitmask |> Array.toList)
-                                |> List.map (fun (v, m) -> match m with
-                                                           | Ignore -> v |> string |> int64
-                                                           | Value x -> x)
-                                |> fun x -> Convert.ToInt64(String.Join("", x), 2))
-                                 emulator.Memory }
+    match emulator.Version with
+    | One -> { emulator with Memory =
+                (ReplaceArrayIdx location
+                (List.zip valueBits (emulator.Bitmask |> Array.toList)
+                    |> List.map (fun (v, m) -> match m with
+                        | Ignore -> v |> string |> int64
+                        | Value x -> x)
+                    |> fun x -> Convert.ToInt64(String.Join("", x), 2)) emulator.Memory) }
+    | Two -> RecMaskedWrite value (ExpandFloatingBits [ (ApplyMask location emulator.Bitmask) ]) emulator
 
 let ReadProgram path =
     File.ReadAllLines path
@@ -59,18 +102,25 @@ let main argv =
         printfn "Path: %s" path
         
         let program = ReadProgram path
-        let maxAddress = program |> List.choose (fun f -> match f with
-                                                  | Write x -> Some x
-                                                  | _ -> None)
-                                 |> List.maxBy (fst) |> fst |> int
         let emulator = {
-            Memory = Array.init (maxAddress + 1) (fun f -> 0L)
+            Memory = []
             Bitmask = [||]
+            Version = One
         }
         
         let resultEmulator = RunProgram emulator program
-        let totalSumLeftInMemory = resultEmulator.Memory |> Array.sum
-        printfn "Total sum left in memory after completion (*): %d" totalSumLeftInMemory
+        let totalSumLeftInMemory = resultEmulator.Memory |> List.sumBy (fun f -> f.Value)
+        printfn "Total sum left in memory after completion with v1 (*): %d" totalSumLeftInMemory
+        
+        let emulatorTwo = {
+            Memory = []
+            Bitmask = [||]
+            Version = Two
+        }
+        
+        let resultEmulatorV2 = RunProgram emulatorTwo program
+        let totalSumLeftInMemoryV2 = resultEmulatorV2.Memory |> List.sumBy (fun f -> f.Value)
+        printfn "Total sum left in memory after completion with v2 (**): %d" totalSumLeftInMemoryV2
         0
     | _ ->
         printfn "Usage: dotnet run ./path/to/input.txt"
