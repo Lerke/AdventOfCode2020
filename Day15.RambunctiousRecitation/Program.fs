ca460bc313fd105bede433a5118693dd79edef55
Willem <willem.sporen@scenius.nl>
2020-12-17 23:49:21 +0100

    Day 15 - Scaffold

---
+++ Day15.RambunctiousRecitation/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| path |] ->
+        printfn "Day 15 - Rambunctious Recitation"
+        printfn "Path: %s" path
+        0
+    | _ ->
+        printfn "Usage: dotnet run ./path/to/input.txt"
+        1
+        
