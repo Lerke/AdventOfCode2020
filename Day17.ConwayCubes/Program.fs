d9ba971866568fbdc5a5568fdb8ea34e33104e78
Willem <willem.sporen@scenius.nl>
2020-12-22 00:04:06 +0100

    Day 17 - Two star

--- Day17.ConwayCubes/Program.fs
+++ Day17.ConwayCubes/Program.fs
@@ -1,4 +1,7 @@
-﻿open System.IO
+﻿open System.Collections.Concurrent
+open System.Diagnostics
+open System.IO
+open FSharp.Collections.ParallelSeq
 
 type CubeState =
     | Inactive
@@ -8,30 +11,41 @@ and PocketDimension = { Cubes: Cube list }
 
 and Cube =
     { State: CubeState
-      Position: (int * int * int) }
+      Position: (int * int * int * int) }
 
-let CubeAtPosition position dimension =
-    match List.tryFind (fun c -> c.Position = position) dimension.Cubes with
-    | Some x -> x
-    | None ->
-        { State = Inactive
-          Position = position }
+let mutable CubeCache =
+    ConcurrentDictionary<(int * int * int * int), Cube>()
 
-let NeighbourCoordinates position =
+let inline CubeAtPosition position dimension =
+    match CubeCache.TryGetValue position with
+    | (true, x) -> x
+    | (false, _) ->
+        match List.tryFind (fun c -> c.Position = position) dimension.Cubes with
+        | Some x -> x
+        | None ->
+            let r =
+                { State = Inactive
+                  Position = position }
+
+            CubeCache.TryAdd(r.Position, r) |> ignore
+            r
+
+let inline NeighbourCoordinates position =
     match position with
-    | (x, y, z) ->
+    | (x, y, z, w) ->
         [ (x - 1) .. (x + 1) ]
         |> List.allPairs [ (y - 1) .. (y + 1) ]
         |> List.allPairs [ (z - 1) .. (z + 1) ]
-        |> List.map (fun (z, (y, x)) -> (x, y, z))
-        |> List.except [ (x, y, z) ]
+        |> List.allPairs [ (w - 1) .. (w + 1) ]
+        |> List.map (fun (w, (z, (y, x))) -> (x, y, z, w))
+        |> List.except [ (x, y, z, w) ]
 
-let NeighbourCubes position dimension =
+let inline NeighbourCubes position dimension =
     NeighbourCoordinates position
     |> List.map (fun p -> CubeAtPosition p dimension)
     |> List.filter (fun p -> p.Position <> position)
 
-let CubesWithState targetState cubes =
+let inline CubesWithState targetState cubes =
     cubes
     |> List.filter (fun f -> f.State = targetState)
     |> List.length
@@ -39,13 +53,14 @@ let CubesWithState targetState cubes =
 let SimulateRound dimension =
     let newCubes =
         (dimension.Cubes
-         |> List.collect (fun f -> NeighbourCubes f.Position dimension)
+         |> PSeq.collect (fun f -> NeighbourCubes f.Position dimension)
+         |> PSeq.toList
          |> List.distinct)
 
     { dimension with
           Cubes =
               newCubes
-              |> List.map (fun c ->
+              |> PSeq.map (fun c ->
                   match (c.State,
                          ((NeighbourCubes c.Position dimension)
                           |> CubesWithState Active),
@@ -54,53 +69,50 @@ let SimulateRound dimension =
                   | (Active, x, y) when x = 2 || x = 3 -> c
                   | (Active, x, y) -> { c with State = Inactive }
                   | (Inactive, 3, y) -> { c with State = Active }
-                  | (Inactive, _, _) -> c) }
+                  | (Inactive, _, _) -> c)
+              |> PSeq.toList }
 
 let PrintDimension dimension =
-    let indices =
-        dimension.Cubes
-        |> List.map (fun d ->
-            match d.Position with
-            | (_, _, z) -> z)
-        |> List.distinct
-        |> List.sortBy id
+    (List.groupBy (fun f ->
+        match f.Position with
+        | (x, y, z, w) -> w) dimension.Cubes)
+    |> List.map (fun f ->
+        List.groupBy (fun fz ->
+            match fz.Position with
+            | (x, y, z, w) -> z) (snd f))
+    |> List.map (fun f ->
+        List.map (fun (w, fw) ->
+            List.groupBy (fun fz ->
+                match fz.Position with
+                | (x, y, z, w) -> y) fw) f)
+    |> (List.iteri (fun iw w ->
+            List.iteri (fun iz z ->
+                printfn ""
 
-    [ (indices |> List.min) .. (indices |> List.max) ]
-    |> List.map (fun i ->
+                printfn
+                    "z=%d w=%d"
+                    (iz
+                     - (((w |> List.length |> double) / 2.0)
+                        |> floor
+                        |> int))
+                    (iw
+                     - (((w |> List.length |> double) / 2.0)
+                        |> floor
+                        |> int))
 
-        let nodes =
-            dimension.Cubes
-            |> List.filter (fun d ->
-                match d.Position with
-                | (_, _, z) -> z = i)
-            |> List.sortBy (fun d ->
-                match d.Position with
-                | (x, y, _) -> (y * 100) + x)
-            |> List.groupBy (fun d ->
-                match d.Position with
-                | (_, y, _) -> y)
+                List.iter (fun y ->
+                    List.iter (fun x ->
+                        match x.State with
+                        | Active -> printf "#"
+                        | Inactive -> printf ".") (snd y)
 
-        nodes)
-    |> List.zip [ (indices |> List.min) .. (indices |> List.max) ]
-    |> fun x -> List.except (List.filter (fun (z, y) -> List.forall (fun fg -> fg.State = Inactive) (List.collect (fun fg -> (snd fg)) y)) x) x
-    |> List.iter (fun (z, y) ->
-        printfn "z=%d" z
-
-        List.iter (fun yy ->
-            List.iter (fun yyy ->
-                match yyy.State with
-                | Inactive -> printf "."
-                | Active -> printf "#") (snd yy)
-
-            printfn "") y
-
-        printfn "")
-
-    printfn ""
+                    printfn "") z) w))
 
 let rec SimulateNTimes N dimensions =
     match N with
     | x when x > 0 ->
+        printfn "Running simulation %d" N
+        CubeCache.Clear()
         SimulateNTimes
             (N - 1)
             ((SimulateRound(List.head dimensions))
@@ -115,10 +127,10 @@ let ParseInput path =
             match y with
             | '.' ->
                 { State = Inactive
-                  Position = (ix, iy, 0) }
+                  Position = (ix, iy, 0, 0) }
             | '#' ->
                 { State = Active
-                  Position = (ix, iy, 0) }
+                  Position = (ix, iy, 0, 0) }
             | _ -> failwithf "Unknown input symbol: %c" y) x
         |> Seq.toList)
     |> List.collect id
@@ -134,7 +146,10 @@ let main argv =
         printfn "Cycles: %d" cycles
 
         let initial = { Cubes = ParseInput path }
+        let sw = Stopwatch()
+        sw.Start()
         let dimensions = SimulateNTimes cycles [ initial ]
+        sw.Stop()
 
         dimensions
         |> List.iteri (fun i x ->
@@ -146,7 +161,7 @@ let main argv =
             |> List.last
             |> fun x -> CubesWithState Active x.Cubes
 
-        printfn "[ * ] Number of cubes left in active state after %d cycles: %d" cycles numberOfActiveStates
+        printfn "[ ** ] Number of cubes left in active state after %d cycles: %d\nSimulation took %d seconds!" cycles numberOfActiveStates sw.Elapsed.Seconds
         0
     | _ ->
         printfn "Usage: dotnet run ./path/to/input.txt"
