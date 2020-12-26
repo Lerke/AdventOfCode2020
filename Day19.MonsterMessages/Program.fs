25aadc416932b100520fcc86a2cd2a75ae4d8944
Willem <willem.sporen@scenius.nl>
2020-12-26 15:54:40 +0100

    Wip DAy 19 - One star

--- Day19.MonsterMessages/Program.fs
+++ Day19.MonsterMessages/Program.fs
@@ -3,14 +3,11 @@ open System.IO
 open System.Text.RegularExpressions
 open FSharp.Text.RegexProvider
 
-type TerminalPattern = Regex< @"(?<ruleId>\d+): \""?(?<ruleChar>([a-z]+\|?)+)\""?">
+type TerminalPattern = Regex< @"(?<ruleId>\d+): \""(?<ruleChars>.*)\""" >
 type NonTerminalPattern = Regex<"(?<ruleId>\d+): (?<rules>\d.*)">
 type InputPattern = Regex<"^(?<rule>[ab]+)">
 type RulePattern = Regex<"^(?<ruleId>\d+): (?<pattern>.*)">
-type Rule = {
-    Index: int
-    Pattern: string
-}
+type Rule = { Index: int; Pattern: string }
 
 let rec ParseRules lines =
     let (terminals, nonTerminals) =
@@ -28,44 +25,70 @@ let rec ParseRules lines =
                        | _ -> false)) with
             | true -> Some f
             | false -> None)
+
     let newTerminals =
         nonterminalsToReplace
-        |> Seq.map (fun f -> sprintf "%s: %s"
-                                 f.ruleId.Value
-                                 (f.rules.Value.Replace(" ", "")
-                                  |> Seq.map (fun r ->  match r with
-                                                        | '"' -> ""
-                                                        | '|' -> (r |> string)
-                                                        | _ -> (terminals
-                                                               |> Seq.find (fun nt -> nt.ruleId.Value = (r |> string))).ruleChar.Value)
-                                                               |> Seq.concat |> Seq.toArray |> String))
+        |> Seq.map (fun f ->
+            sprintf
+                "%s: \"%s\""
+                f.ruleId.Value
+//                (f.rules.Value.Replace(" ", "")
+                 (f.rules.Value.Split(" ")
+                 |> Seq.map (fun r ->
+                     match r with
+                     | "\"" -> "\""
+                     | "|" -> "|"
+                     | _ ->
+                         sprintf
+                             "(%s)"
+                             (terminals
+                              |> Seq.find (fun nt -> nt.ruleId.Value = (r |> string)))
+                                 .ruleChars.Value)
+                 |> Seq.concat
+                 |> Seq.toArray
+                 |> String))
         |> Seq.append (terminals |> Seq.map (fun x -> x.Value))
-        
+
     let newNonterminals =
         nonTerminals
         |> Seq.except nonterminalsToReplace
         |> Seq.map (fun f -> f.Value)
-        
-    let newResults =  (Seq.append newNonterminals newTerminals) 
+
+    let newResults =
+        (Seq.append newNonterminals newTerminals)
+
     match Seq.isEmpty newNonterminals with
-    | false -> ParseRules (String.Join("\n", newResults))
-    | true -> newResults
-              |> Seq.map (fun f -> f.Replace("\"", ""))
-              |> Seq.map (fun f -> match RulePattern().TryTypedMatch f with
-                                   | Some s -> { Pattern = s.pattern.Value
-                                                 Index = s.ruleId.Value |> int }
-                                   | None -> failwithf "Could not parse input: %s" f)
+    | false -> ParseRules(String.Join("\n", newResults))
+    | true ->
+        newResults
+        |> Seq.map (fun f -> f.Replace("\"", ""))
+        |> Seq.map (fun f ->
+            match RulePattern().TryTypedMatch f with
+            | Some s ->
+                { Pattern = "^" + s.pattern.Value + "$"
+                  Index = s.ruleId.Value |> int }
+            | None -> failwithf "Could not parse input: %s" f)
+
+let ApplyPartTwoRules lines =
+    Regex.Replace(Regex.Replace(lines, "^8:.*", "8: 42 | 42 8"), "11:.*", "11: 42 31 | 42 11 31");
 
 [<EntryPoint>]
 let main argv =
     match argv with
     | [| path |] ->
         let rules = ParseRules(File.ReadAllText path)
-        let inputLines = InputPattern(RegexOptions.Multiline).TypedMatches (File.ReadAllText path) |> Seq.map (fun f -> f.rule.Value)
+
+        let inputLines =
+            InputPattern(RegexOptions.Multiline)
+                .TypedMatches(File.ReadAllText path)
+            |> Seq.map (fun f -> f.rule.Value)
+
         let RuleWithIdxZero = rules |> Seq.find (fun f -> f.Index = 0)
-        
+
         let linesMatchingRuleZero =
-            inputLines |> Seq.map (fun f -> Regex.IsMatch(f, $"^({RuleWithIdxZero.Pattern})$"), f)
+            inputLines
+            |> Seq.map (fun f -> Regex.IsMatch(f, $"^({RuleWithIdxZero.Pattern})$", (RegexOptions.Multiline)), f)
+        printfn "[ * ] Number of rules completely matching rule 0: %d" (linesMatchingRuleZero |> Seq.filter (fun (m, p) -> m = true) |> Seq.length )
         0
     | _ ->
         printfn "Usage: dotnet run ./path/to/input.txt"
